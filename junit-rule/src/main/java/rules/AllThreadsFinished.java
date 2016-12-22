593b77047483fef0ec04ae040fbaf93d5d9346c5
Andrey Tonkikh <xosmig@gmail.com>
2016-12-22 10:35:06 +0300

    junit-rule. Check uncaught exception

--- junit-rule/src/main/java/rules/AllThreadsFinished.java
+++ junit-rule/src/main/java/rules/AllThreadsFinished.java
@@ -4,25 +4,30 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 
 /**
- * This rule will automatically check that all threads, registered via <code>register</code> method call, are finished.
+ * This rule will automatically check that all threads, registered via <code>register</code> method call,
+ * are finished without exceptions.
  */
 public class AllThreadsFinished implements TestRule {
 
-    private List<Thread> threads = new ArrayList<>();
+    private final Map<Thread, Throwable> uncaught = new HashMap<>();
 
+    private List<Thread> threads = new ArrayList<>();
+    private Thread.UncaughtExceptionHandler exceptionHandler = uncaught::put;
 
     /**
      * Register new thread.
      */
     public void register(Thread thread) {
-        this.threads.add(thread);
+        threads.add(thread);
+//        if (thread.getUncaughtExceptionHandler() == null) {
+            thread.setUncaughtExceptionHandler(exceptionHandler);
+//        }
     }
 
     @Override
@@ -32,6 +37,11 @@ public class AllThreadsFinished implements TestRule {
             public void evaluate() throws Throwable {
                 base.evaluate();
                 for (Thread thread : threads) {
+                    Throwable e = uncaught.get(thread);
+                    if (e != null) {
+                        throw new AssertionError("Uncaught exception in thread \"" + thread.getName() + "\"", e);
+                    }
+
                     assertTrue("Thread \"" + thread.getName() + "\" isn't terminated.",
                                 thread.getState() == Thread.State.TERMINATED);
                 }
--- junit-rule/src/test/java/rules/AllThreadsFinishedTest.java
+++ junit-rule/src/test/java/rules/AllThreadsFinishedTest.java
@@ -13,9 +13,8 @@ public class AllThreadsFinishedTest {
 
     @Rule
     public RuleChain rules = RuleChain
-            .outerRule(this.expectedException)
-            .around(this.threadsCheckRule);
-
+            .outerRule(expectedException)
+            .around(threadsCheckRule);
 
     @Test
     public void testOk() throws Exception {
@@ -24,7 +23,7 @@ public class AllThreadsFinishedTest {
         for (int i = 0; i < n; i++) {
             th[i] = new Thread(this::doSomething);
             th[i].start();
-            this.threadsCheckRule.register(th[i]);
+            threadsCheckRule.register(th[i]);
         }
         for (int i = 0; i < n; i++) {
             th[i].join();
@@ -32,24 +31,37 @@ public class AllThreadsFinishedTest {
     }
 
     @Test
-    public void testFail() throws Exception {
-        this.expectedException.expect(AssertionError.class);
-        this.expectedException.handleAssertionErrors();  // note: deprecated since JUnit 4.12
+    public void testNotFinished() throws Exception {
+        expectedException.expect(AssertionError.class);
+        expectedException.handleAssertionErrors();  // note: deprecated since JUnit 4.12
 
         final int n = 5;
         Thread[] th = new Thread[n];
         for (int i = 0; i < n; i++) {
             th[i] = new Thread(this::doSomething);
             th[i].start();
-            this.threadsCheckRule.register(th[i]);
+            threadsCheckRule.register(th[i]);
         }
     }
 
     @Test
-    public void testEmpty() throws Exception {
-        this.doSomething();
+    public void testExceptionOccurred() throws Exception {
+        expectedException.expect(AssertionError.class);
+        expectedException.handleAssertionErrors();
+
+        Thread th = new Thread(() -> {
+            doSomething();
+            throw new RuntimeException("Hello, World!");
+        });
+        threadsCheckRule.register(th);
+        th.start();
+        th.join();
     }
 
+    @Test
+    public void testEmpty() throws Exception {
+        doSomething();
+    }
 
     volatile long r;
 
